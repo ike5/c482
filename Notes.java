52d8e75f7d2a605b4406ed025655ec3f82d4011a
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:30:08 -0700

    floating-point common errors

--- Notes.java
+++ Notes.java
@@ -2,10 +2,9 @@ public class Notes{
     public static void main(String[] args) {
         
         // floating-point data types
-        // DEFAULT IS DOUBLE
 
-        float f = 29.75F; // single precision => 7 digits of accuracy
-
-        double d = 29.75; // double precision => 15 digits of accuracy
+        // Strongly typed, so will cause error
+        float number;
+        number = 43.54;
     }
 }
