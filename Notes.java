ec861a897b0c25633e31ae2084835f120dfcdb6c
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:23:11 -0700

    primitive data types

--- Notes.java
+++ Notes.java
@@ -1,10 +1,24 @@
 public class Notes{
     public static void main(String[] args) {
         
-        // Specific rules for identifiers:
+        // primitive data types
 
-        //1) first character can be any of the following: a-z, A-Z, _, $
-        //2) no spaces
-        //3) case matters
+        byte b1 = -128; // 1 byte
+        byte b2 = 127;
+
+        short s1 = -32768; // 2 bytes
+        short s2 = 32767;
+
+        int i1 = -2147483648; // 4 bytes
+        int i2 = 2147483647; 
+
+        long l1 = -9223372036854775808; // 8 bytes
+        long l2 = 9223372036854775807;
+
+        float f1 = 3.4e-38; // 4 bytes
+        float f2 = 3.4e38; 
+
+        double d1 = 1.7e-308; // 8 bytes
+        double d2 = 1.7e308;
     }
 }
