8f94b3e841cd29e9e5fa0c6dbbd1ecf3aba1cf39
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:27:50 -0700

    floating-point data types

--- Notes.java
+++ Notes.java
@@ -1,24 +1,11 @@
 public class Notes{
     public static void main(String[] args) {
         
-        // primitive data types
+        // floating-point data types
+        // DEFAULT IS DOUBLE
 
-        byte b1 = -128; // 1 byte
-        byte b2 = 127;
+        float f = 29.75F; // single precision => 7 digits of accuracy
 
-        short s1 = -32768; // 2 bytes
-        short s2 = 32767;
-
-        int i1 = -2147483648; // 4 bytes
-        int i2 = 2147483647; 
-
-        long l1 = -9223372036854775808L; // 8 bytes
-        long l2 = 9223372036854775807L;
-
-        float f1 = 3.4e-38f; // 4 bytes
-        float f2 = 3.4e38f; 
-
-        double d1 = 1.7e-308; // 8 bytes
-        double d2 = 1.7e308;
+        double d = 29.75; // double precision => 15 digits of accuracy
     }
 }
