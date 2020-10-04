8f709c2b2135e57b0af41bd7b6ba5384fc16b164
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:12:21 -0700

    example of value and literal

--- Notes.java
+++ Notes.java
@@ -1,8 +1,9 @@
 public class Notes{
     public static void main(String[] args) {
         
-        // variable vs literal
-        // A variable is a named storage location 
-        // A literal is a value that is written into the code of the program.
+        int value;
+        value = 5;
+        System.out.print("The value is ");
+        System.out.println(value);
     }
 }
