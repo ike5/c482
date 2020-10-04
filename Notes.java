dbab1cc5d528a0c6a70954d1da0d544950518793
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:44:38 -0700

    Math class

--- Notes.java
+++ Notes.java
@@ -1,10 +1,16 @@
 public class Notes{
     public static void main(String[] args) {
         
-        // integer division
+        // Math class
 
-        // remainder discarded, result is original type, but without remainder
-        double number; number = 5/2;
-        System.out.print(number);
+        double result,x,sqrt;
+        result = Math.pow(4.0, 2.0);
+        x = 3 * Math.pow(6.0, 3.0);
+        sqrt = Math.sqrt(9.0);
+
+
+        System.out.println("result:\t" + result);
+        System.out.println("x:\t" + x);
+        System.out.println("sqrt:\t" + sqrt);
     }
 }
