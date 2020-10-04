353842ce6f986318d5ea7b3a97f684f56422a8f5
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:01:01 -0700

    Initial commit

---
+++ Notes.java
@@ -0,0 +1,8 @@
+public class Notes{
+    public static void main(String[] args) {
+        
+        // THE PRINT AND PRINTLN METHODS
+        System.out.print("Hello World!");
+        System.out.print("Hello Again, but notice no space.");
+    }
+}
