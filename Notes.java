b9aeaea006933e1f713c1b23dfb1534794b31a70
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:17:09 -0700

    rules for identifiers

--- Notes.java
+++ Notes.java
@@ -1,7 +1,10 @@
 public class Notes{
     public static void main(String[] args) {
         
-        // Identifiers:
-        // Variable names and class names are examples
+        // Specific rules for identifiers:
+
+        //1) first character can be any of the following: a-z, A-Z, _, $
+        //2) no spaces
+        //3) case matters
     }
 }
