e61831cd3090c7e5739296d9fb2793df8459589d
Ike Maldonado <maldonadoike@gmail.com>
2020-10-04 11:24:23 -0700

    updated primitive data type

--- Notes.java
+++ Notes.java
@@ -12,11 +12,11 @@ public class Notes{
         int i1 = -2147483648; // 4 bytes
         int i2 = 2147483647; 
 
-        long l1 = -9223372036854775808; // 8 bytes
-        long l2 = 9223372036854775807;
+        long l1 = -9223372036854775808L; // 8 bytes
+        long l2 = 9223372036854775807L;
 
-        float f1 = 3.4e-38; // 4 bytes
-        float f2 = 3.4e38; 
+        float f1 = 3.4e-38f; // 4 bytes
+        float f2 = 3.4e38f; 
 
         double d1 = 1.7e-308; // 8 bytes
         double d2 = 1.7e308;
